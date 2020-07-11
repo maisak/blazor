3d67887e8f85800a52fbc27bf43ae638c4f4b922
Anthony <anthony.maisak@hotmail.com>
2020-07-11 18:28:43 +0300

    Basic culture feature.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Globalization;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.JSInterop;
 
 namespace SampleBlazorApp.Client
 {
@@ -13,7 +15,7 @@ namespace SampleBlazorApp.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-
+            
             builder.Services.AddHttpClient("SampleBlazorApp.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
@@ -21,6 +23,18 @@ namespace SampleBlazorApp.Client
             builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("SampleBlazorApp.ServerAPI"));
 
             builder.Services.AddApiAuthorization();
+
+            // localization
+            builder.Services.AddLocalization();
+            var host = builder.Build();
+            var jsInterop = host.Services.GetRequiredService<IJSRuntime>();
+            var result = await jsInterop.InvokeAsync<string>("blazorCulture.get");
+            if (result != null)
+            {
+                var culture = new CultureInfo(result);
+                CultureInfo.DefaultThreadCurrentCulture = culture;
+                CultureInfo.DefaultThreadCurrentUICulture = culture;
+            }
 
             await builder.Build().RunAsync();
         }
