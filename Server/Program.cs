a1a52bfa0093b1375e26de8c7023168a02264719
Anthony <anthony.maisak@hotmail.com>
2020-10-11 11:03:20 +0300

    Migrate database at startup.

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,19 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using SampleBlazorApp.Server.Data.Security;
+using System.Threading.Tasks;
+using SampleBlazorApp.Server.Data.Image;
 
 namespace SampleBlazorApp.Server
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var serviceScope = host.Services.CreateScope())
+            {
+                var serviceProvider = serviceScope.ServiceProvider;
+                await using var securityContext = serviceProvider.GetRequiredService<SecurityDbContext>();
+                await using var imageContext = serviceProvider.GetRequiredService<ImageDbContext>();
+                await securityContext.Database.MigrateAsync();
+                await imageContext.Database.MigrateAsync();
+            }
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
